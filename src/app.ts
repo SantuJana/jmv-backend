c8c0b1cd6b22219f9ba2e502d6f96a03c7718ebb
Santu Jana <santujana.1998@gmail.com>
2026-06-03 00:24:16 +0530

    production swagger fix

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,7 @@ export const createApp = () => {
           defaultSrc: ["'self'"],
           scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
           styleSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
+          connectSrc: ["'self'", "https://cdn.jsdelivr.net"],
           imgSrc: ["'self'", "data:", "https://cdn.jsdelivr.net"]
         }
       }
